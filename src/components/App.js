ecfd011600b20ac526eeb08ab3c6e660114a4fbf
Toren Wallengren <toren.wallengren@addepar.com>
2018-12-30 17:18:22 -0700

    added comments for clarity

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,32 @@
+// import necessary libraries
 import React, { Component } from 'react'
 import axios from 'axios'
 
+// url used for server
 const BASE_URL = "http://localhost:3005"
 
+// Main App - Root of component tree
 class App extends Component {
 
     constructor() {
 
         super()
 
+        // set state variables to track user & bot messages
         this.state = {
+
+            // current user input - should update on any change to input
             userInput: "",
+
+            // current bot reply - should update when send message button is clicked
             currentReply: "",
+
+            // messages array - keeps track of message history
             messages: [
                 { user: 'bot', message: 'hello' },
                 { user: 'toren', message: 'greetings' },
-            ]
+            ],
+
         }
 
     }
@@ -36,9 +47,26 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <input placeholder="enter your message to bot" onChange={(event) => this.updateUserInput(event)} />
-                <button onClick={() => { this.getBotReply(this) }} >SEND MESSAGE</button>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "100vw",
+                }}>
+
+                <input
+                    style={{
+                        width: "100%",
+                    }}
+                    placeholder="enter your message to bot"
+                    onChange={(event) => this.updateUserInput(event)} />
+
+                <button
+                    onClick={() => { this.getBotReply(this) }} >
+                    SEND MESSAGE
+                </button>
+
                 {this.state.currentReply}
             </div>
         )
